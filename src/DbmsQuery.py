fdc0de09dab102352616c2372b357c09570f3402
Roberto <roberto.barile98@gmail.com>
2020-02-07 11:02:00 +0100

    cypher examples

--- src/DbmsQuery.py
+++ src/DbmsQuery.py
@@ -1,11 +1,14 @@
 from src.IGraphDBQuery import IGraphDBQuery
+from py2neo import Graph
 
 class DbmsQuery(IGraphDBQuery):
-    def __init__(self, query):
-        self.query = query
+    def __init__(self, query, password):
+        self.__query = query
+        self.__password = password
+        self.__graph = Graph(self.__password)
 
     def run_query(self):
-        return
+        return self.__graph.run(self.__query).to_table()
 
     def set_query(self, query):
-        self.query = query
+        self.__query = query
