fe5f9b74e4c8d17cc98da84c0e60b6e0bcf350e7
Sergio Caputo <sergiocaputo1998@gmail.com>
2020-02-07 20:07:31 +0100

    commit #1

--- src/CloudQuery.py
+++ src/CloudQuery.py
@@ -1,12 +1,35 @@
 from src.IGraphDBQuery import IGraphDBQuery
+from SPARQLWrapper import SPARQLWrapper, JSON  # pip install sparqlwrapper
+from src.Data import Data
 
 
 class DbmsQuery(IGraphDBQuery):
-    def __init__(self, query):
-        self.query = query
+
+    def __init__(self, query, dataset):
+        self.__query = query
+        self.__dataset = dataset
+        self.__sparql = SPARQLWrapper(dataset)
 
     def run_query(self):
-        return
+        self.__sparql.setReturnFormat(JSON)
+        results = self.__sparql.query().convert()
+        triple = []
+        length = 0
+        for results in results["results"]["bindings"]:
+            triple.append(results["p"]["value"])
+            triple.append(results["o"]["value"])
+            triple.append(results["s"]["value"])
+            length = length + 1
+        data = Data(triple, length)
+        return data
 
     def set_query(self, query):
-        self.query = query
+        self.__query = query
+        self.__sparql.setQuery(query)
+
+    def get_query(self):
+        return self.__query
+
+    def get_dataset(self):
+        return self.__dataset
+
