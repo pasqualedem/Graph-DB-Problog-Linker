93b3b7e5e4145e65a752e516f26f9d08b110d899
Sergio Caputo <sergiocaputo1998@gmail.com>
2020-02-08 12:34:17 +0100

    Tuples added to triples

--- src/CloudQuery.py
+++ src/CloudQuery.py
@@ -13,14 +13,12 @@ class CloudQuery(IGraphDBQuery):
     def run_query(self):
         self.__sparql.setReturnFormat(JSON)
         results = self.__sparql.query().convert()
-        triple = []
+        triples = []
         length = 0
         for results in results["results"]["bindings"]:
-            triple.append(results["p"]["value"])
-            triple.append(results["o"]["value"])
-            triple.append(results["s"]["value"])
+            triples.append((results["s"]["value"], results["p"]["value"], results["o"]["value"]))
             length = length + 1
-        data = Data(triple, length)
+        data = Data(triples, length)
         return data
 
     def set_query(self, query):
