9a62eefbec6e514951300e319d44eacbc9ca6c76
Roberto <roberto.barile98@gmail.com>
2020-02-12 18:35:17 +0100

    removed useless comments

--- test/QueryTest.py
+++ test/QueryTest.py
@@ -1,33 +1,6 @@
 import unittest
 from src.Query import CloudQuery, DbmsQuery
 
-"""
-dbmsQuery = DbmsQuery("MATCH(n:Person) OPTIONAL MATCH (n:Person)-[r]->(c:Car) RETURN ID(n), properties(n), TYPE(r), "
-                      "ID(c), properties(c)",
-                      "parse_node_rels_with_props_map",
-                      "test")
-
-print(dbmsQuery.run_query())
-
-dbmsQuery = DbmsQuery("MATCH(n:Person) OPTIONAL MATCH (n:Person)-[r]->(c:Car) RETURN "
-                      "size(keys(n)), ID(n), n.name AS name, n.age AS age,"
-                      "TYPE(r),"
-                      "size(keys(c)), ID(c), c.brand as brand, c.targa as targa",
-                      "parse_node_rels_with_props_array",
-                      "test")
-
-print(dbmsQuery.run_query())
-
-dbmsQuery = DbmsQuery("MATCH(n:Person) OPTIONAL MATCH (n:Person)-[r]->(c:Car) RETURN ID(n), TYPE(r), ID(c)",
-                      "parse_node_rels",
-                      "test")
-
-print(dbmsQuery.run_query())
-
-
-        """
-
-
 class DbmsQueryTest(unittest.TestCase):
     def test_parse_props_array(self):
         test = DbmsQuery("MATCH (n:Person) RETURN ID(n), n.name AS name, n.age AS age",
