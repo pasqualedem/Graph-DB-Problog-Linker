d4dd054d86e8402b6c5be96217f7fe83fb61a557
Roberto <roberto.barile98@gmail.com>
2020-02-12 16:00:02 +0100

    parsing da dizionario di proprietà a triple, interfaccia IGraphDBQuery spostata nel file Query

--- test/QueryTest.py
+++ test/QueryTest.py
@@ -48,18 +48,21 @@ from src.Query import CloudQuery
 
 class Test:
     def __init__(self):
-        self.query = "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> PREFIX dbp: <http://dbpedia.org/property/>" \
-                     " SELECT ?cheese ?label WHERE {  ?cheese a <http://dbpedia.org/ontology/Cheese>; " \
-                     " dbp:region <http://dbpedia.org/resource/Asturias> ;          rdfs:label ?label .}"
+        self.query = "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> " \
+                     "PREFIX dbp: <http://dbpedia.org/property/>" \
+                     "SELECT ?subject ?label " \
+                     "WHERE {  " \
+                     "  ?subject a <http://dbpedia.org/ontology/Cheese>; " \
+                     "  dbp:region <http://dbpedia.org/resource/Asturias> ;          " \
+                     "  rdfs:label ?label." \
+                     "}"
+
         self.dataset = "http://dbpedia.org/sparql"
         self.t = CloudQuery(self.query, self.dataset)
-        print(self.t.get_query())
-        print(self.t.get_dataset())
 
     def run(self):
         data = self.t.run_query()
-        for triple in data.get_triples():
-            print(triple)
+
 
 test = Test()
 test.run()
