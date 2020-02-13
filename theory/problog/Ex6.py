db97064c231da0ca274e2e3b73b437d6414f27ed
Pasquale De Marinis <pasquale199876@hotmail.it>
2020-02-13 16:44:28 +0100

    correzione

--- theory/problog/Ex6.py
+++ theory/problog/Ex6.py
@@ -12,7 +12,7 @@ from problog import get_evaluatable
 coin,heads,tails,win,query = Term('coin'),Term('heads'),Term('tails'),Term('win'),Term('query')
 C = Var('C')
 p = SimpleProgram()
-p += coin(Constant('http://dbpedia.org/resource/Annot_(artist)'))
+p += coin(Constant('c1'))
 p += coin(Constant('c2'))
 p += AnnotatedDisjunction([heads(C,p=0.4), tails(C,p=0.6)], coin(C))
 p += (win << heads(C))
