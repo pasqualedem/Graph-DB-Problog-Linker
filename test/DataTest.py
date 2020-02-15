f20b4a4398bb20bb07e080201c75b5fbbdfead76
Roberto <roberto.barile98@gmail.com>
2020-02-15 12:21:19 +0100

    learn distribution test

--- test/DataTest.py
+++ test/DataTest.py
@@ -1,7 +1,9 @@
 import unittest
-from Data import Data
+from Data import Data, PropertyMap, Property
 from problog import get_evaluatable
 from problog.logic import Term, Var, Constant
+
+from Distribution import Multinomial
 
 
 class MyTestCase(unittest.TestCase):
@@ -46,6 +48,36 @@ class MyTestCase(unittest.TestCase):
         print(dic.keys())
         print(dic.values())
         print(dic)
+
+    def test_to_program(self):
+        data = Data(
+            [[(0, 'OWNS', 55), (0, 'brand', 'Ford'), (0, 'targa', '123'), (55, 'name', 'Luigi'), (55, 'age', 11)],
+             [(1, 'OWNS', 17), (1, 'brand', 'Seat'), (1, 'targa', '124'), (17, 'name', 'Sergio'), (17, 'age', 18)],
+             [(15, 'OWNS', 19), (15, 'name', 'Giuseppe'), (15, 'age', 19), (19, 'brand', 'Suzuki'), (19, 'targa', '143')],
+             [(15, 'KNOWS', 55), (15, 'name', 'Giuseppe'), (15, 'age', 19), (55, 'name', 'Luigi'), (55, 'age', 11)],
+             [(15, 'KNOWS', 17), (15, 'name', 'Giuseppe'), (15, 'age', 19), (17, 'name', 'Sergio'), (17, 'age', 18)],
+             [(15, 'KNOWS', 16), (15, 'name', 'Giuseppe'), (15, 'age', 19), (16, 'name', 'Pasquale'), (16, 'age', 17)],
+             [(16, 'OWNS', 21), (16, 'name', 'Pasquale'), (16, 'age', 17), (21, 'brand', 'Audi'), (21, 'targa', '127')],
+             [(16, 'KNOWS', 15), (16, 'name', 'Pasquale'), (16, 'age', 17), (15, 'name', 'Giuseppe'), (15, 'age', 19)],
+             [(16, 'KNOWS', 17), (16, 'name', 'Pasquale'), (16, 'age', 17), (17, 'name', 'Sergio'), (17, 'age', 18)],
+             [(17, 'OWNS', 1), (17, 'name', 'Sergio'), (17, 'age', 18), (1, 'brand', 'Seat'), (1, 'targa', '124')],
+             [(17, 'KNOWS', 15), (17, 'name', 'Sergio'), (17, 'age', 18), (15, 'name', 'Giuseppe'), (15, 'age', 19)],
+             [(17, 'KNOWS', 16), (17, 'name', 'Sergio'), (17, 'age', 18), (16, 'name', 'Pasquale'), (16, 'age', 17)],
+             [(18, 'KNOWS', 55), (18, 'name', 'Roberto'), (18, 'age', 15), (55, 'name', 'Luigi'), (55, 'age', 11)],
+             [(19, 'OWNS', 15), (19, 'brand', 'Suzuki'), (19, 'targa', '143'), (15, 'name', 'Giuseppe'), (15, 'age', 19)],
+             [(20, 'brand', 'Fiat'), (20, 'targa', '125')],
+             [(21, 'OWNS', 16), (21, 'brand', 'Audi'), (21, 'targa', '127'), (16, 'name', 'Pasquale'), (16, 'age', 17)],
+             [(55, 'OWNS', 0), (55, 'name', 'Luigi'), (55, 'age', 11), (0, 'brand', 'Ford'), (0, 'targa', '123')],
+             [(55, 'KNOWS', 15), (55, 'name', 'Luigi'), (55, 'age', 11), (15, 'name', 'Giuseppe'), (15, 'age', 19)],
+             [(55, 'KNOWS', 18), (55, 'name', 'Luigi'), (55, 'age', 11), (18, 'name', 'Roberto'), (18, 'age', 15)],
+             [(56, 'KNOWS', 57), (56, 'name', 'Matteo'), (57, 'name', 'Francesco')],
+             [(57, 'KNOWS', 56), (57, 'name', 'Francesco'), (56, 'name', 'Matteo')]], 21)
+
+        property_map = PropertyMap()
+        property_map["brand"] = Property("brand", Multinomial())
+
+        property_map = data.learn_distributions(property_map)
+        property_map.to_simple_program()
 
 if __name__ == '__main__':
     unittest.main()
